a8b82baacd77d3bf7c0bcbc8e13b8d2e30f00b79
kunvar13 <kunvar13@gmail.com>
2021-11-09 00:20:23 -0600

    Adding function to generate the string and saving the new urls to the urlDatabase

--- express_server.js
+++ express_server.js
@@ -1,8 +1,20 @@
 const express = require("express");
 const app = express();
 const PORT = 8080; // default port 8080
-
+const bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
+
+const generateRandomString = function() {
+  let result = '';
+  let characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+  let charactersLength = characters.length;
+  for (let i = 0; i < 6; i++) {
+    result += characters.charAt(Math.floor(Math.random() *
+charactersLength));
+  }
+  return result;
+};
 
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
@@ -32,6 +44,14 @@ app.get("/urls/new", (req, res) => {
 app.get("/urls/:shortURL", (req, res) => {
   const templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL] };
   res.render("urls_show", templateVars);
+});
+
+app.post("/urls", (req, res) => {
+  console.log(req.body);
+  let sUrl = generateRandomString();
+  urlDatabase[sUrl] = (req.body.longURL);  // Log the POST request body to the console
+  res.send(sUrl);
+  console.log(urlDatabase);         // Respond with 'Ok' (we will replace this)
 });
 
 
